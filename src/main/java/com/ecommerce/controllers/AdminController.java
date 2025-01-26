0f71caeeb7e786f89997b109b3983916fe00ca25
Tushar Khushal Mahajan <mahajantushar208@gmail.com>
2025-01-26 11:13:30 +0530

    ADDED BASIC VALIDATIONS ON add-prod.html page AND ALSO ADD VALIDATIONS FOR EMPTY IMAGE OBJECT

--- src/main/java/com/ecommerce/controllers/AdminController.java
+++ src/main/java/com/ecommerce/controllers/AdminController.java
@@ -1,10 +1,6 @@
 package com.ecommerce.controllers;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ecommerce.entities.Product;
@@ -35,25 +30,35 @@ public class AdminController {
 
 	@GetMapping
 	public String admin(Model model) {
-		model.addAttribute("product", new Product());
-		return "admin/add-prod";
+		return "redirect:admin/add-product";
 	}
 
 	@GetMapping("/add-product")
-	public String addProducts(Model model) {
+	public String addProducts(@RequestParam(name = "status", required = false, defaultValue = "EMPTY") String status,
+			Model model) {
 
+		System.out.println("status = :" + status);
+
+		model.addAttribute("STATUS", status);
 		model.addAttribute("product", new Product());
 		return "admin/add-prod";
 	}
 
 	@PostMapping("/Saveproduct")
-	@ResponseBody
-	public String saveProduct(@ModelAttribute("product") Product product, @RequestParam("img") MultipartFile file)
-			throws IOException {
+	public String saveProduct(@ModelAttribute("product") Product product, @RequestParam("img") MultipartFile file,
+			Model model) throws IOException {
+
+		StringBuffer parameter = new StringBuffer("EMPTY");
 
 		try {
+
+			if (file.isEmpty()) {
+				throw new Exception("File Object is empty");
+			}
+
 			System.out.println("SAVED PRODUCT = :" + product);
 
+			System.out.println("empty :" + file.isEmpty());
 			// for s3
 			FileHelper fileHelper = new FileHelper(s3Service);
 			String fileName = fileHelper.saveImageAndReturnImageNameInS3(file);
@@ -62,15 +67,21 @@ public class AdminController {
 			product.setProductImage(fileName);
 			if (productService.insertProduct(product) != null) {
 				// SUCCESS
-				return "product save";
+				parameter.setLength(0);
+				parameter.append("SAVE");
+
 			} else {
 				// SOMETHING WRONG
-				return "something went wrong";
+				parameter.setLength(0);
+				parameter.append("ERR");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "something went wrong";
+			parameter.setLength(0);
+			parameter.append("ERR");
 		}
+
+		return "redirect:add-product?status=" + parameter.toString();
 
 	}
 }
